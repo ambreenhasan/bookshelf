c877850dd83bcf997a2f42b6195d92fa8fe417f4
Ambreen Hasan <ambodumbo@gmail.com>
2014-11-09 14:53:37 -0800

    added bcrypt and validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,17 @@
+require 'bcrypt'
+
 class User < ActiveRecord::Base
+  include BCrypt
+
+  validates :name, presence: true
+  validates :email, presence: true
+  validates :password, presence: true
+
+  validates :email, uniqueness: true
+  validates :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i, on: :create }
+
+  validates :password, length: { in: 6..20 }
+
   has_many :book_ownerships
   has_many :books, through: :book_ownerships
 
@@ -7,6 +20,15 @@ class User < ActiveRecord::Base
 
   has_many :followers, through: :follower_relationships
   has_many :followeds, through: :followed_relationships
+
+  def password
+    @password ||= Password.new(password_hash)
+  end
+
+  def password=(new_password)
+    @password = Password.create(new_password)
+    self.password_hash = @password
+  end
 
   def follow user
     Relationship.create follower_id: self.id, followed_id: user.id
