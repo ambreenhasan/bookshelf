7931d23342314980c9d89012100004f22ea94059
Ambreen Hasan <ambodumbo@gmail.com>
2014-11-09 11:52:43 -0800

    added column for book review

--- db/migrate/20141109104157_create_bookownerships.rb
+++ db/migrate/20141109104157_create_bookownerships.rb
@@ -3,6 +3,7 @@ class CreateBookownerships < ActiveRecord::Migration
     create_table :bookownerships do |t|
       t.belongs_to :user
       t.belongs_to :book
+      t.string :book_review
       t.integer :current_page
       t.boolean :completed, default: false
 
