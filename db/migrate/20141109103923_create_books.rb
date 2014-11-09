4ebc0d3edcd9a1830f07b2b64e7d3abb60057a39
Ambreen Hasan <ambodumbo@gmail.com>
2014-11-09 11:15:09 -0800

    added migration tables for users and book tables

--- db/migrate/20141109103923_create_books.rb
+++ db/migrate/20141109103923_create_books.rb
@@ -1,4 +1,11 @@
 class CreateBooks < ActiveRecord::Migration
   def change
+    create_table :books do |t|
+      t.string :title
+      t.string :author
+      t.string :book_cover
+
+      t.timestamps
+    end
   end
 end
