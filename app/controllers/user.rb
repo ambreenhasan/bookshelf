6842f4fb7d0b16b2db97e3d9ca5bf8ec4881fade
Ambreen Hasan <ambodumbo@gmail.com>
2014-11-10 21:48:07 -0800

    finished currently reading routes, fix completed button though

--- app/controllers/user.rb
+++ app/controllers/user.rb
@@ -6,15 +6,23 @@ get '/account/currently_reading' do
   erb :currently_reading
 end
 
+get '/account/currently_reading/books/:book_id/edit' do
+  @books = current_user.book_ownerships.find_by_book_id(params[:book_id])
+  erb :current_progress
+end
+
 # [PUT] current user can update the current page of a book they are currently reading
-put'/account/currently_reading/:book_id' do
+put'/account/currently_reading/books/:book_id/edit' do
   user = current_user.book_ownerships.find_by_book_id(params[:book_id])
   user.update_attributes(current_page: params[:current_page])
   redirect '/account/currently_reading'
 end
 
+get '/account/currently_reading/books/:book_id/completed' do
+  @books = current_user.book_ownerships.find_by_book_id(params[:book_id])
+end
 
-put '/account/currently_reading/:book_id/completed' do
+put '/account/currently_reading/books/:book_id/completed' do
   user = current_user.book_ownerships.find_by_book_id(params[:book_id])
   user.update_attributes(completed: true)
   redirect '/account/currently_reading'
